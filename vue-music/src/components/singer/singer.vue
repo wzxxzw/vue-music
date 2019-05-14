ea2836c6feff78284f09016382507520db93ae13
Your Name <you@example.com>
2019-05-15 07:53:25 +0800

    77

--- vue-music/src/components/singer/singer.vue
+++ vue-music/src/components/singer/singer.vue
@@ -4,8 +4,13 @@
   </div>
 </template>
 <script type="text/ecmascript-6">
-import {getSingerList} from 'api/singer'
-import {ERR_OK} from 'api/config'
+  import {getSingerList} from 'api/singer'
+  import {ERR_OK} from 'api/config'
+  import Singer from 'common/js/singer'
+
+
+  const HOT_SINGER_LEN = 10
+  const HOT_NAME = '热门'
 export default {
   data () {
     return {
@@ -20,11 +25,54 @@ export default {
       getSingerList().then((res) => {
         if (res.code === ERR_OK) {
           console.log(res)
-          // this.singerList = res.data.slider
-          // console.log(this.recommends)
+          this.singerList = res.data.list
+          console.log(this._normalizeSinger(this.singerList))
         }
       })
     },
+  _normalizeSinger(list) {
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        }
+        list.forEach((item, index) => {
+          if (index < HOT_SINGER_LEN) {
+            map.hot.items.push(new Singer({
+              name: item.Fsinger_name,
+              id: item.Fsinger_mid
+            }))
+          }
+          console.log(map)
+          const key = item.Findex
+          if (!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            }
+          }
+          map[key].items.push(new Singer({
+            name: item.Fsinger_name,
+            id: item.Fsinger_mid
+          }))
+        })
+        // 为了得到有序列表，我们需要处理 map
+        let ret = []
+        let hot = []
+        for (let key in map) {
+          let val = map[key]
+          if (val.title.match(/[a-zA-Z]/)) {
+            ret.push(val)
+          } else if (val.title === HOT_NAME) {
+            hot.push(val)
+          }
+        }
+        ret.sort((a, b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+        })
+        return hot.concat(ret)
+      },
   }
 }
 
