3d2302921601c2c81fc092c043fc90c63e3ae5bc
Your Name <you@example.com>
2019-05-22 07:38:17 +0800

    gheshou

--- vue-music/src/base/listview.vue
+++ vue-music/src/base/listview.vue
@@ -1,36 +1,40 @@
 <template>
-    <div>
-       <scroll  class="listview" :data="data">
-         <div>
-            <ul>
-              <li v-for="(group, index) in data" class="list-group" ref="listGroup" :key="index">
-                  <h2 class="list-group-title">{{ group.title }}</h2>
-                  <uL>
-                    <li v-for="(item, index) in group.items" class="list-group-item" :key="index">
-                      <img class="avatar" v-lazy="item.avatar">
-                      <span class="name">{{item.name}}</span>
-                    </li>
-                  </uL>
+    <scroll  
+    class="listview" 
+    :data="data"
+    ref="listview">
+      <ul>
+        <li v-for="(group, index) in data" class="list-group" ref="listGroup" :key="index">
+            <h2 class="list-group-title">{{ group.title }}</h2>
+            <uL>
+              <li v-for="(item, index) in group.items" class="list-group-item" :key="index">
+                <img class="avatar" v-lazy="item.avatar">
+                <span class="name">{{item.name}}</span>
               </li>
-            </ul>
-            <div class="list-shortcut">
-              <ul>
-                <li v-for="(item, index) in shortcutList" :data-index="index" class="item"
-                    :key="index"
-                    :class="{'current':currentIndex===index}">{{item}}
-                </li>
-              </ul>
-            </div>
-            <div class="list-fixed" ref="fixed" v-show="fixedTitle">
-              <div class="fixed-title">{{fixedTitle}} </div>
-            </div>
-          </div>
-       </scroll>
-    </div>
+            </uL>
+        </li>
+      </ul>
+      <div class="list-shortcut" @touchstart.stop.prevent="onShortcutTouchStart" @touchmove.stop.prevent="onShortcutTouchMove"
+         @touchend.stop>
+        <ul>
+          <li v-for="(item, index) in shortcutList" :data-index="index" class="item"
+              :key="index"
+              :class="{'current':currentIndex===index}">{{item}}
+          </li>
+        </ul>
+      </div>
+      <div class="list-fixed" ref="fixed" v-show="fixedTitle">
+        <div class="fixed-title">{{fixedTitle}} </div>
+      </div>
+    </scroll>
+  
 </template>
  <script>
 import BScroll from 'better-scroll'
 import Scroll from 'base/scroll'
+import {getData} from 'common/js/dom'
+  const TITLE_HEIGHT = 30
+  const ANCHOR_HEIGHT = 18
  export default {
   props: {
     data: {
@@ -40,8 +44,21 @@ import Scroll from 'base/scroll'
       }
     }
   },
+  data() {
+    return {
+      scrollY: -1,
+      currentIndex: 0,
+      diff: -1
+    }
+  },
   components: {
     Scroll
+  },
+  created() {
+    this.probeType = 3
+    this.listenScroll = true
+    this.touch = {}
+    this.listHeight = []
   },
   computed: {
     shortcutList() {
@@ -49,7 +66,43 @@ import Scroll from 'base/scroll'
         return group.title.substr(0, 1)
       })
     },
-  }  
+    fixedTitle() {
+      if (this.scrollY > 0) {
+        return ''
+      }
+      return this.data[this.currentIndex] ? this.data[this.currentIndex].title : ''
+    }
+  },
+  methods: {
+      onShortcutTouchStart(e) {
+        let anchorIndex = getData(e.target, 'index')
+        let firstTouch = e.touches[0]
+        this.touch.y1 = firstTouch.pageY
+        this.touch.anchorIndex = anchorIndex
+
+        this._scrollTo(anchorIndex)
+      },
+      onShortcutTouchMove(e) {
+        let firstTouch = e.touches[0]
+        this.touch.y2 = firstTouch.pageY
+        let delta = (this.touch.y2 - this.touch.y1) / ANCHOR_HEIGHT | 0
+        let anchorIndex = parseInt(this.touch.anchorIndex) + delta
+
+        this._scrollTo(anchorIndex)
+      },
+      _scrollTo(index) {
+        if (!index && index !== 0) {
+          return
+        }
+        if (index < 0) {
+          index = 0
+        } else if (index > this.listHeight.length - 2) {
+          index = this.listHeight.length - 2
+        }
+        this.scrollY = -this.listHeight[index]
+        this.$refs.listview.scrollToElement(this.$refs.listGroup[index], 0)
+      }
+  }
  }
  </script>
  <style scoped lang="stylus" rel="stylesheet/stylus">
@@ -83,7 +136,7 @@ import Scroll from 'base/scroll'
           color: $color-text-l
           font-size: $font-size-medium
     .list-shortcut
-      position: fixed
+      position: absolute
       z-index: 30
       right: 0
       top: 50%
